d1f6cdec7566f89c8c5da9250522f807ffa10cec
Jeremy Evert <jeremy.evert@swosu.edu>
2015-10-30 07:43:30 -0500

    I have a simple method call working.

--- src/MethodPractice.java
+++ src/MethodPractice.java
@@ -1,6 +1,26 @@
 public class MethodPractice {
 	public static void main(String[] args) {
 		System.out.println("Hello this is a mac.");
+		
+		// Define a variable called name.
+		String name = "steve";
+		
+		// Pass the name to a method that makes a greeting.
+		// Keep the greeting as a string.
+		String greetings = HelloSteve(name);
+		
+		// print the greeting.
+		System.out.println(greetings);
+	}
+	
+	public static String HelloSteve (String name){
+		String greetings;
+		String frontOfGreeting = "Hello ";
+		String backOfGreeting = ", nice to meet you.";
+		
+		greetings = frontOfGreeting + name + backOfGreeting;
+		
+		return greetings;
 	}
 
 }
